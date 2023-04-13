e62dd3466c0f0d5360b4326258c103e86193ce9f
Minh03-code <21211tt4504@mail.tdc.edu.vn>
2023-04-13 21:04:18 +0700

    create get user by id

---
+++ DoAnBE2/resources/views/auth/showlist.blade.php
@@ -0,0 +1,29 @@
+@extends('dashboard')
+@section('content')
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+            </tr>
+        </thead>
+        <tbody>
+
+            @foreach ($list as $key => $user)
+                <tr>
+                    <th scope="row">{{$user->id}}</th>
+                    
+                    <td><a href="{{ route('show-detail', ['id'=>$user->id])}}">{{$user->name}}</a></td>
+                    <td>{{$user->email}}</td>
+                </tr>
+            @endforeach
+
+        </tbody>
+    </table>
+    <div class="container" style="display: flex;">
+        <div class="d-felx justify-content-center">
+            {{ $list->links('pagination::bootstrap-4') }}
+        </div>
+    </div>
+@endsection
